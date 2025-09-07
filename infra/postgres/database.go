bc0e59b9e99cf091c813be923663d5f576ad0479
Farhan1033 <hanzoi1006@gmail.com>
2025-09-07 07:03:12 +0700

    Update database connection string to require SSL and remove timezone parameter

--- infra/postgres/database.go
+++ infra/postgres/database.go
@@ -17,13 +17,12 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=%s",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
 		config.Get("DB_HOST"),
 		config.Get("DB_USER"),
 		config.Get("DB_PASS"),
 		config.Get("DB_NAME"),
 		config.Get("DB_PORT"),
-		config.Get("TIMEZONE"),
 	)
 
 	var err error
