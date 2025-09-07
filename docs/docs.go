a0147e29312daa573c17ad21be4bda6f6d258df9
Farhan1033 <hanzoi1006@gmail.com>
2025-09-07 07:41:25 +0700

    Remove HTTPS from host URLs in Swagger documentation

--- docs/docs.go
+++ docs/docs.go
@@ -2083,7 +2083,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "https://movieticket-farhan10335643-qxvhtr05.leapcell.dev:8080",
+	Host:             "movieticket-farhan10335643-qxvhtr05.leapcell.dev:8080",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Movie Ticket API",
